f647a9a4f8366540dbd0b770940100d5a0e57ebf
Szymon Jachim <szymon.jachim@ocado.com>
2016-10-25 11:36:14 +0200

    Simple single cell tests.

--- gol.py
+++ gol.py
@@ -1,12 +1,33 @@
 #!/bin/python
 
 class Board:
+  somethingIsAlive = False
+
   def isCellAlive(self, x, y):
-    pass 
+    return self.somethingIsAlive
+
+  def bringToLife(self, x, y):
+    self.somethingIsAlive = True
+
+  def evolve(self):
+    self.somethingIsAlive = False
 
 def createdBoardHasDeadCell():
   board = Board()
   assert not board.isCellAlive(0, 0)
   
+def canBringOneCellToLife():
+  board = Board()
+  board.bringToLife(0, 0)
+  assert board.isCellAlive(0, 0)
+  
+def singleCellDiesOfLonelyness():
+  board = Board()
+  board.bringToLife(0, 0)
+  board.evolve()
+  assert not board.isCellAlive(0, 0)
+  
 
 createdBoardHasDeadCell()
+canBringOneCellToLife()
+singleCellDiesOfLonelyness()
